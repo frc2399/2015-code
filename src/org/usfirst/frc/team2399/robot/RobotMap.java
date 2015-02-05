d142db4bf891bc5dd1b0686d1fb7b4829b0f517f
chloe6248 <cschwartz17@hb.edu>
2015-02-04 21:01:07 -0500

    InitialElevatorCode

--- src/org/usfirst/frc/team2399/robot/RobotMap.java
+++ src/org/usfirst/frc/team2399/robot/RobotMap.java
@@ -1,4 +1,9 @@
 package org.usfirst.frc.team2399.robot;
+
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -6,6 +11,16 @@ package org.usfirst.frc.team2399.robot;
  * floating around.
  */
 public class RobotMap {
+	
+	public static final int ELEVATOR_JAGUARID=1; //est elevator jaguar (all caps is recommended) and sets it = to 1; in reality, =1 is unlikely
+	public static final int ELEVATOR_ENCODERPORT_A=0; 
+	public static final int ELEVATOR_ENCODERPORT_B=1; 
+	public static final int SWITCHPORT=2; //assigned to ports
+	
+	public static final CANJaguar ELEVATOR_JAGUAR= new CANJaguar(ELEVATOR_JAGUARID); //assigns which jaguar
+	public static final Encoder ELEVATOR_ENCODER=new Encoder(ELEVATOR_ENCODERPORT_A,ELEVATOR_ENCODERPORT_B);//assigns encoder to ports
+	public static final DigitalInput ELEVATOR_SWITCH=new DigitalInput(SWITCHPORT);//assigns switch to ports
+	
     // For example to map the left and right motors, you could define the
     // following variables to use with your drivetrain subsystem.
     // public static int leftMotor = 1;
@@ -16,3 +31,7 @@ public class RobotMap {
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
 }
+//wants you to declare constants for ports and speed controllers
+//void is for methods that have no return
+//static= only one
+//final= cannot change
---
+++ src/org/usfirst/frc/team2399/robot/subsystems/Elevator.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team2399.robot.subsystems;
+
+import org.usfirst.frc.team2399.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+// things to do: subsystems and commands for subsystems; elevator should be
+// done; do drive train; make a mecanum drive obj so you can use the library
+// method for if limit switch is pressed, driving motor
+// speed; config encoders, possibly jaguars
+public class Elevator extends Subsystem {
+	private CANJaguar elevatorMotor;// private sets them so that they're only
+									// visible in this class
+	private Encoder elevatorEncoder;
+	private DigitalInput elevatorSwitch;
+
+	public Elevator() {
+		elevatorMotor = RobotMap.ELEVATOR_JAGUAR;//sets elevator motor= to the one created in RobotMap
+		elevatorEncoder=RobotMap.ELEVATOR_ENCODER;
+		elevatorSwitch= RobotMap.ELEVATOR_SWITCH;
+	}
+
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+
+}
