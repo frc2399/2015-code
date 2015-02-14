04ec9194b7d25569540c5e7d20c641297f084df0
Lauren Egts <lauren.egts@gmail.com>
2015-02-14 12:45:42 -0500

    put encoder stuff in DriveForward

--- src/org/usfirst/frc/team2399/robot/commands/DriveTrainForward.java
+++ src/org/usfirst/frc/team2399/robot/commands/DriveTrainForward.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc.team2399.robot.commands;
 import org.usfirst.frc.team2399.robot.subsystems.*;
 import org.usfirst.frc.team2399.robot.Robot;
+import org.usfirst.frc.team2399.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
@@ -11,7 +13,10 @@ public class DriveTrainForward extends Command{
 	private DriveTrain driveTrain = Robot.driveTrain;
 	private Gyro gyro;
 	private double twist;
-//	private double a;
+	private CANJaguar LEFTFRONT_JAGUAR = RobotMap.LEFTFRONT_JAGUAR;
+    private DigitalInput contactSwitchOne = Robot.contactSwitchOne;
+    private DigitalInupt contactSwitchFour = Robot.contactSwitchFour;
+	//private double a;
 //	private double v;
 //	private double vPrev = 0;
 //	private double x;
@@ -23,6 +28,7 @@ public class DriveTrainForward extends Command{
 	
 	public DriveTrainForward(){
 		requires(Robot.driveTrain);
+		//requires(RobotMap.LEFTFRONT_JAGUAR);
 		//acceleromter = new Accelerometer(RobotMap.accelerometer);
 	}
 
@@ -37,7 +43,54 @@ public class DriveTrainForward extends Command{
 	//sp = your angle that you want to be at
 	//sp-pv = E
 	protected void execute() {
-		driveTrain.driveRobotOriented(0, 0.25, (twist = 1*(0-gyro.getAngle())));
+		int driveTo = 12; //TODO: get the actual numbers
+		//driveTo is the number of rotations required to get to the tote plus two
+		//two more rotations in case of under/over shooting and human error
+		double finishPoint = (LEFTFRONT_JAGUAR.getPosition() + driveTo);
+		if (finishPoint > 0){
+			driveTrain.driveRobotOriented(0, 0.25, (twist = 1*(0-gyro.getAngle())));
+		} else if (contactSwitchOne.get() == true && contactSwitchFour.get() == true){
+			/*
+			 * the else if is going to drive forward until the contact switches 1 and 4 have been triggered
+			 * when they have been triggered, they SHOULD cause the robot to break out of this if statement
+			 * and into the next thing in the command group
+			 * if it doesn't do the thing then we need to find the command that makes it move onto the next 
+			 * command
+			 * THE CODE IS MAD RIGHT NOW
+			 * that is because the contact switch code doesn't exist in this branch
+			 * when this branch is merged into SubsystemCreation2015 the code should be happy
+			 */
+			break; //
+		} else if (LEFTFRONT_JAGUAR.getPosition() >= driveTo){
+			/*
+			 * this pretty much says if the position of the robot is greater then or euqal to the
+			 * point where it should have gone then stop driving and go on to the next command 
+			 * in the command group
+			 * a bit of a fail-safe in case the contact switches don't trigger
+			 * also would probably be useful in DriveAutoZone to make sure the robot doesn't 
+			 * drive out of auto zone
+			 * AGAIN
+			 * break SHOULD make the robot move onto the next command in the command group
+			 * if it doesn't, we need to find what DOES
+			 * because this setup right now is really pretty and I like it
+			 */
+			break;
+		}
+		
+		
+		
+		/*
+		 * complete psuedocode here:
+		 * use encoders somehow like this?
+		 * reset encoder
+		 * if encoder is displaying a value less then whatever distance we want {
+		 * go forward;
+		 * } else {
+		 * break;
+		 * }
+		 * sort of like the 2014 autoposition execute loop
+		 * but with encoders instead of ultrasonic
+		 */
 	}
 
 	@Override
